83c2820253de426a500e23118912b2952deb3264
btishkovets@gmail.com <mwg3936Bogdan>
2022-07-14 21:38:08 +0300

    Add test1

---
+++ src/Lesson1/Home.java
@@ -0,0 +1,14 @@
+package Lesson1;
+
+public class Home {
+    public static void main(String[] args) {
+        System.out.println();
+
+    }
+
+
+    public void  test1(){
+        System.out.println("test");
+
+    }
+}
